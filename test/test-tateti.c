a0941e3feecf1c78964d3eaf41fcc67b8aec4ef5
Silvio <silvioq@gmail.com>
2010-03-20 08:47:37 -0300

    Mejora el test

--- test/test-tateti.c
+++ test/test-tateti.c
@@ -12,13 +12,32 @@
 #include <unistd.h>
 #include <assert.h>
 #include <qgames.h>
-#include "<qgames_analyzer.h>"
+#include <qgames_analyzer.h>
 
 
 int  main(int argc, char** argv) {
-    int  ret;
+    Tipojuego* tateti;
     char *filename = "../games/TaTeTi.qgame";
-    assert( ret = qgz_parse( NULL, filename, 0 ) );
+    assert( tateti = qgz_parse_filename( filename, 0 ) );
+    printf( "." );
 
+    assert( tipojuego_get_casillero( tateti, "a4" ) == NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "a1" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "a2" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "a3" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "b1" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "b2" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "b3" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "c1" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "c2" ) != NOT_FOUND );
+    assert( tipojuego_get_casillero( tateti, "c3" ) != NOT_FOUND );
+    printf( "." );
+
+    assert( tipojuego_get_color( tateti, "rojo" ) == NOT_FOUND );
+    assert( tipojuego_get_color( tateti, "blanco" ) == 1 );
+    assert( tipojuego_get_color( tateti, "negro" )  == 2 ) ;
+    printf( "." );
+
+    printf( "\n" );
     return  EXIT_SUCCESS;
 }
