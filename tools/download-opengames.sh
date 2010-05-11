584357acae3c462a6bca9c6cbdeb41d345feab5f
Silvio <silvioq@gmail.com>
2010-05-11 13:10:56 -0300

    Mas test

--- tools/download-opengames.sh
+++ tools/download-opengames.sh
@@ -6,6 +6,11 @@ if [ $1x == x ]; then
 fi
 
 juego=$1
+tjuego=$2
+if [ "$tjuego" == "" ]; then
+    tjuego="Ajedrez"
+fi
+
 file=/tmp/$$.tmp
 
 curl -sf http://www.opengames.com.ar/{$juego}.pgn > $file
@@ -18,14 +23,14 @@ fi
 
 echo "# Juego obtenido de  http://www.opengames.com.ar/${juego}.pgn"
 echo "# Entre " `cat $file | grep White | sed "s/White //g"` "y" `cat $file | grep Black | sed "s/Black //g"`
-echo "nuevo-juego Ajedrez"
+echo "nuevo-juego $tjuego"
 cat $file | grep -v "^\\["
 # cat $file | grep "Result" | sed s/[^-012\\*\\/]//g
 
 rm $file
 
 curl -sf http://www.opengames.com.ar/es/{$juego} > $file
-for linea in `cat $file | grep "\".*-.*-.*\"" | grep -v \< | grep -v captured`; do
+for linea in `cat $file | grep "\".*-.*-.*\"" | grep -v \< | grep -v captured | grep -v pozo`; do
     pieza=`echo $linea | sed "s/-/ /g" | sed s/\"//g | sed s/,//g`
     echo  "check-piezas $pieza 1"
 done
