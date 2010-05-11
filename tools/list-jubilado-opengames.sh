82c3e55cc9cf228877739cc440f297a24be37406
Silvio <silvioq@gmail.com>
2010-05-11 13:32:26 -0300

    Ajedrez jubilado y sus test

---
+++ tools/list-jubilado-opengames.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+function  list_page(){
+    curl -sf "http://www.opengames.com.ar/es/room/Ajedrez-Jubilado/history?page=$1" | \
+    awk "/http:\\/\\/www.opengames.com.ar\\/es\\/[0-9]+/ { match( \$0, \"[0-9]+\" ); print substr(\$0, RSTART, RLENGTH)  }" 
+}
+
+i=1
+while true; do
+    salida=`list_page $i`
+    if [ "$salida" == "" ]; then
+        break
+    fi
+    echo pagina: $i ">" $salida
+    for partida in $salida; do
+        file="../test/qgtest/opengames-$partida.qgtest"
+        if [ -f $file ]; then
+            echo "$file existente"
+        else
+            printf "generando $file"
+            ./download-opengames.sh $partida Jubilado > $file
+            echo " ... generado"
+            sleep 4
+        fi
+    done
+    i=$[$i + 1]
+done
+
