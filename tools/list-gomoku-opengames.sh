2228a4f8c28f704658660ff9fc5f0ca3df8e1575
Silvio <silvioq@gmail.com>
2010-09-23 17:33:57 -0300

    Nuevos test

--- tools/list-gomoku-opengames.sh
+++ tools/list-gomoku-opengames.sh
@@ -4,14 +4,13 @@ function  list_page(){
     awk "/http:\\/\\/www.opengames.com.ar\\/es\\/[0-9]+/ { match( \$0, \"[0-9]+\" ); print substr(\$0, RSTART, RLENGTH)  }" 
 }
 
-i=1
-while true; do
-    salida=`list_page $i`
-    if [ "$salida" == "" ]; then
-        break
-    fi
-    echo pagina: $i ">" $salida
-    for partida in $salida; do
+function  list_torneo(){
+    curl -sf "http://www.opengames.com.ar/es/tournaments/2010/April%20Open%20Gomoku%20Tournament" | \
+    awk "/http:\\/\\/www.opengames.com.ar\\/es\\/[0-9]+/ { match( \$0, \"[0-9]+\" ); print substr(\$0, RSTART, RLENGTH)  }" 
+}
+
+function  procesar_salida(){
+    for partida in $1; do
         file="../test/qgtest/opengames-$partida.qgtest"
         if [ -f $file ]; then
             echo "$file existente"
@@ -22,6 +21,22 @@ while true; do
             sleep 4
         fi
     done
+  
+}
+
+salida=`list_torneo`
+echo torneo ">" $salida
+procesar_salida $salida
+
+i=1
+while true; do
+    salida=`list_page $i`
+    if [ "$salida" == "" ]; then
+        break
+    fi
+    echo pagina: $i ">" $salida
+    procesar_salida "$salida"
     i=$[$i + 1]
 done
+
 
