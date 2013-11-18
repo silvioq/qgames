cdeb3274928a0bd19119919a3115e62cbc2c6e7d
SilvioQ <silvioq@gmail.com>
2013-11-17 22:06:23 -0300

    Test mas profundos

--- test/bin/test-generic.c
+++ test/bin/test-generic.c
@@ -39,10 +39,82 @@ int  main( int argc, char** argv ){
     // Este test simple controla la no duplicidad de las direcciones
     assert( tj = qg_tipojuego_new( "Generico" ) );
     qg_tipojuego_add_direccion( tj, "norte" );
+    qg_tipojuego_add_color( tj, "blanco" );
     assert( qg_tipojuego_valido( tj ) );
     qg_tipojuego_add_casillero( tj, "n" );
     assert( qg_tipojuego_valido( tj ) );
+    qg_tipojuego_add_tipo_mov ( tj, "Tipo mov" );
+    qg_tipojuego_add_tipopieza( tj, "Gemagenerica" );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( qg_tipojuego_add_tpieza_att( tj, "Gemagenerica", "att", 1 ) );
+    assert( qg_tipojuego_valido( tj ) );
+
+    assert( qg_tipojuego_add_pieza( tj, "Gemagenerica", "n", "blanco" ) );
+    assert( qg_tipojuego_valido( tj ) );
+
+    // error direccion
     qg_tipojuego_add_direccion( tj, "norte" );
+    assert( !qg_tipojuego_valido( tj ) );
+
+    // test de error de pieza
+    assert( tj = qg_tipojuego_new( "Generico" ) );
+    qg_tipojuego_add_color( tj, "blanco" );
+    qg_tipojuego_add_casillero( tj, "x" );
+    qg_tipojuego_add_tipopieza( tj, "Gemagenerica" );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( qg_tipojuego_add_pieza( tj, "Gemagenerica", "x", "blanco" ) );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( !qg_tipojuego_add_pieza( tj, "Gemagenerica", "xx", "blanco" ) );
+    assert( !qg_tipojuego_valido( tj ) );
+
+    // controlo posible error de atributo
+    assert( tj = qg_tipojuego_new( "Generico" ) );
+    qg_tipojuego_add_tipopieza( tj, "Gemagenerica" );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( !qg_tipojuego_add_tpieza_att( tj, "Gemaquenoexiste", "att", 1 ) );
+    assert( !qg_tipojuego_valido( tj ) );
+
+    // controlo zonas
+    assert( tj = qg_tipojuego_new( "Generico" ) );
+    assert( qg_tipojuego_add_zona( tj, "zona" ) );
+    qg_tipojuego_add_casillero( tj, "a1" );
+    qg_tipojuego_add_casillero( tj, "a2" );
+    qg_tipojuego_add_color( tj, "verde" );
+    qg_tipojuego_add_color( tj, "negro" );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( qg_tipojuego_add_cas_to_zona( tj, "a1", "verde", "zona" ) );
+    assert( qg_tipojuego_valido( tj ) );
+
+    assert( !qg_tipojuego_add_cas_to_zona( tj, "a1", "azul", "zona" ) );  // Falla por color
+    assert( !qg_tipojuego_valido( tj ) );
+ 
+    assert( tj = qg_tipojuego_new( "Generico" ) );
+    assert( qg_tipojuego_add_zona( tj, "zona" ) );
+    qg_tipojuego_add_casillero( tj, "a1" );
+    qg_tipojuego_add_casillero( tj, "a2" );
+    qg_tipojuego_add_color( tj, "verde" );
+    qg_tipojuego_add_color( tj, "negro" );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( qg_tipojuego_add_cas_to_zona( tj, "a1", "verde", "zona" ) );
+    assert( qg_tipojuego_valido( tj ) );
+
+    assert( !qg_tipojuego_add_cas_to_zona( tj, "a3", "negro", "zona" ) ); // Falla por casillero
+    assert( !qg_tipojuego_valido( tj ) );
+
+    // controlo secuencias
+    assert( tj = qg_tipojuego_new( "Generico" ) );
+    qg_tipojuego_add_casillero( tj, "a1" );
+    qg_tipojuego_add_casillero( tj, "a2" );
+    qg_tipojuego_add_color( tj, "verde" );
+    qg_tipojuego_add_color( tj, "negro" );
+    assert( qg_tipojuego_add_tipo_mov( tj, "tmov" ) );
+    assert( qg_tipojuego_add_secuencia( tj, "verde", NULL ) );
+    assert( qg_tipojuego_add_secuencia( tj, "verde", "tmov" ) );
+    assert( qg_tipojuego_add_secuencia( tj, "negro", NULL ) );
+    assert( qg_tipojuego_add_secuencia( tj, "verde", "tmov" ) );
+    assert( qg_tipojuego_add_secuencia_rep( tj ) );
+    assert( qg_tipojuego_valido( tj ) );
+    assert( !qg_tipojuego_add_secuencia( tj, "azul", NULL ) );
     assert( !qg_tipojuego_valido( tj ) );
 
     exit( EXIT_SUCCESS );
