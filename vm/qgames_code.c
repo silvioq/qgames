8ac99031e0b7627b10ba8fc9301918a89f0ccc70
Silvio <silvioq@gmail.com>
2013-11-26 12:05:38 -0300

    No es necesario controlar salida

--- vm/qgames_code.c
+++ vm/qgames_code.c
@@ -321,8 +321,6 @@ int         tipojuego_code_entablero( Tipojuego* tj ){
     qcode_op( tj->qcode, QCPSH,  3, 0 );        // PSH r3
     qcode_opnlab( tj->qcode, QCCLX, "entablero" );
     return 1;
-    // FIXME: Hay que controlar posibles estados erroneos
-    // RET_IF_STATUS;                              // Retorna si el valor es distinto de cero
 }
 
 int         tipojuego_code_jaquemate( Tipojuego* tj, char* tpieza  ){
